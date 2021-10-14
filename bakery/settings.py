b0a6484f03ff31c7b85999999728c82d112bd959
Ayush <ayushms@gmail.com>
2021-10-15 00:57:44 +0530

    Made some changes

--- bakery/settings.py
+++ bakery/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,7 +33,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'baker',
-    'customers',
+    'customers.apps.CustomersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,11 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'root')
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, '/static/'),
-]
+STATIC_ROOT=os.path.join(BASE_DIR, "static")
+STATIC_DIRS=os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
